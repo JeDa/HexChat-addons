b8633be42a22f58611408d7d9ea0e9f23e8a869d
Jesús "JeDa" Hernández <jedayoshi@gmail.com>
2015-09-25 15:33:03 -0430

    pytest will not work. :P

--- pytest.py
+++ pytest.py
@@ -7,12 +7,21 @@ You ***SHOULDN'T*** load this on your HexChat. :)
 if __name__ == "__main__":
     import os
     import sys
-    import pytest
-    returnlist = []
+    broken = {}
     for val in os.listdir("addons"):
-        returnlist.append(pytest.main(["addons/{0}".format(val), "-s", '--tb', 'native']))
-    if 1 in returnlist:
-        print("There's a broken addon.")
+        try:
+            __import__('addons.{0}'.format(val), globals={"__name__": __name__})
+            print("{0} = WORKING".format(val))
+            broken[val] = 0
+        except:
+            print("{0} = BROKEN".format(val))
+            broken[val] = 1
+    broke = 0
+    for i, val in broken:
+        if val == 1:
+            broke = 1
+    if broke == 1:
+        print("There's broken addons. :(")
         sys.exit(1)
     else:
         print("All is fine. :)")
