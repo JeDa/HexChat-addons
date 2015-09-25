ca6a84c0e968acfde621f6c5f0e3a75a2bca10c8
Jesús "JeDa" Hernández <jedayoshi@gmail.com>
2015-09-25 16:19:55 -0430

    :P

--- pytest.py
+++ pytest.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
     import sys
     broken = 0
     print("HexChat addons test script.")
-    print("There might been shown weird stuff. Don't worry, just ignore them. :P")
+    print("There might been shown weird stuff. Don't worry. :)\n")
     for val in os.listdir("addons"):
-        print("{0} = TESTING".format(val))
+        print("Testing {0}".format(val))
         try:
             __import__('addons.{0}'.format(val.replace(".py", "")), globals={"__name__": __name__})
-            print("{0} = WORKING".format(val))
+            print("{0} is WORKING.".format(val))
         except Exception as err:
-            print("{0} = BROKEN ({1})".format(val, err))
+            print("{0} is FAILING. ({1})".format(val, err))
             broken = 1
     if broken == 1:
         print("\nThere's broken addons. :(")
