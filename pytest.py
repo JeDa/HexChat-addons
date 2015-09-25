5b61ee2468ff37735d1312c07d34cba4c19d69fd
Jesús "JeDa" Hernández <jedayoshi@gmail.com>
2015-09-25 15:41:39 -0430

    boop

--- pytest.py
+++ pytest.py
@@ -7,20 +7,15 @@ You ***SHOULDN'T*** load this on your HexChat. :)
 if __name__ == "__main__":
     import os
     import sys
-    broken = {}
+    broken = 0
     for val in os.listdir("addons"):
         try:
             __import__('addons.{0}'.format(val), globals={"__name__": __name__})
             print("{0} = WORKING".format(val))
-            broken[val] = 0
         except:
             print("{0} = BROKEN".format(val))
-            broken[val] = 1
-    broke = 0
-    for i in broken:
-        if i == 1:
-            broke = 1
-    if broke == 1:
+            broken = 1
+    if broken == 1:
         print("There's broken addons. :(")
         sys.exit(1)
     else:
