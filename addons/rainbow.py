4839b1f9bda80d8e049efda32cf9f1e362118b0e
Jesús "JeDa" Hernández <jedayoshi@gmail.com>
2015-09-25 14:25:08 -0430

    Rainbow 1.0.1

---
+++ addons/rainbow.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import hexchat
+import random
+
+__module_name__ = "Rainbow"
+__module_author__ = "JeDa"
+__module_version__ = "1.0.1"
+__module_description__ = "Generates rainbow-colored versions of the text you enter."
+
+def TheRainbow(word, type):
+	colorlist = ["\x034","\x037","\x038","\x039","\x0311","\x0312","\x0313","\x036","\x034"]
+	rainbowed = ""
+	if type = "chan":
+    	split_text = list(" ".join(word).replace(word[0] + " ", ""))
+	elif type = "privmsg":
+    	split_text = list(" ".join(word).replace(word[0] + " " + word[1] + " ", ""))
+	for i in split_text:
+	    rainbowed += random.choice(colorlist) + i
+	return rainbowed
+
+def rainbow(word,word_eol,userdata):
+	hexchat.command("say " + TheRainbow(word, "chan"))
+
+def rainbowme(word,word_eol,userdata):
+	hexchat.command("me " + TheRainbow(word, "chan"))
+
+def rainbowmsg(word,word_eol,userdata):
+	hexchat.command("msg " + word[1] + " " + TheRainbow(word, "privmsg"))
+
+def rainbownotice(word,word_eol,userdata):
+	hexchat.command("notice " + word[1] + " " + TheRainbow(word, "privmsg"))
+
+hexchat.hook_command("r", rainbow, help="/r (text)")
+hexchat.hook_command("rme", rainbowme, help="/rme (text)")
+hexchat.hook_command("rmsg", rainbowmsg, help="/rmsg (user) (text)")
+hexchat.hook_command("rnotice", rainbownotice, help="/rnotice (user) (text)")
+print("{0} module version {1} by {2} loaded.".format(__module_name__, __module_version__, __module_author__))
